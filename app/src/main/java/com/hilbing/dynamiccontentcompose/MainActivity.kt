e2821ab09b2fd8e7af121dbc413308186e9e8aaf
Gretel Hilbing <ghilbing@gmail.com>
2021-08-17 16:39:58 -0700

    Recomposition state, adding list state

--- app/src/main/java/com/hilbing/dynamiccontentcompose/MainActivity.kt
+++ app/src/main/java/com/hilbing/dynamiccontentcompose/MainActivity.kt
@@ -1,61 +1,67 @@
 package com.hilbing.dynamiccontentcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.hilbing.dynamiccontentcompose.ui.theme.DynamicContentComposeTheme
 
 
-val namesList: ArrayList<String> = arrayListOf("Gretel", "Dany", "Sol", "Alma")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-          GreetingList(names = namesList)
+          MainScreen()
         }
     }
 }
 
 @Composable
-fun GreetingList(names: List<String>){
+fun MainScreen(){
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        for(name in names){
-            Greeting(name = name)
-        }
-        Button(onClick = {namesList.add("New name")}){
-            Text("Add new name")
-        }
+        GreetingList()
     }
 
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun GreetingList(){
+    val greetingListState = remember {mutableStateListOf<String>("Gretel", "Dany")}
+    for(name in greetingListState){
+        Greeting(name = name)
+    }
+    Button(onClick = {greetingListState.add("Sol")}){
+        Text("Add new name")
+    }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        style = MaterialTheme.typography.h4
+        style = MaterialTheme.typography.h5
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-  GreetingList(names = namesList)
+  MainScreen()
 }
